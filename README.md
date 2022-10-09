c85a292eebf4c70f1de59807d3e44227cf018206
nrgxtra <nrgxtra@hotmail.com>
2022-10-09 20:25:39 +0300

    added cart view in unsubscribe success

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Final-Project
+# Final-Project 
+1. install requirements.txt
+2. setup postgres data base  and email in .env file
+3. migrate
+4. create superuser
+5. check trough database field active to activate superuser
+6. create trough admin group "writers" where blog post authors should be added
+7. also create group "store managers" where users who can manipulate shopping app should be added
+8. create categories and tags in apropriate apps troug admin
+9. users with a.m. rights can create, edit, delete posts and add, update, and delete store products trough site itself
+10. You may add some gallery photos at least 8 in order to populate about/gallery section, also posts for /about/blog section and services for about/servise section
+
--- sisys/newsletters_app/views.py
+++ sisys/newsletters_app/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from newsletters_app.models import NewsletterUser
+from shopping_app.utils import get_context_attributes
 
 
 @login_required
@@ -15,12 +16,14 @@ def newsletter_signup(request):
 def newsletter_signout(request):
     user_mail = request.user.email
     subscriber = NewsletterUser.objects.all().get(email=user_mail)
+    context_data = get_context_attributes(request, request.user)
     if subscriber:
         if request.method == 'POST':
             subscriber.delete()
             return redirect('unsubscribe success')
         context = {
             'user': subscriber,
+            'cart_items': context_data['cart_items'],
         }
         return render(request, 'newsletter/unsubscribe-confirmation.html', context)
     else:
@@ -29,3 +32,11 @@ def newsletter_signout(request):
 
 class UnsubscribeSuccessView(TemplateView):
     template_name = 'newsletter/unsubscribe-success.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.request.user
+        context_data = get_context_attributes(self.request, self.request.user)
+        context['user'] = user
+        context['cart_items'] = context_data['cart_items']
+        return context
