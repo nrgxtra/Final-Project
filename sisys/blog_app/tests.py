fe1482f141f1afdbb64ae1d8a1d39bfbfaf02efd
nrgxtra <nrgxtra@hotmail.com>
2022-12-09 16:48:22 +0200

    added som tests

--- sisys/blog_app/tests.py
+++ sisys/blog_app/tests.py
@@ -1,3 +1,40 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from blog_app.models import Post
+from sisis_auth.models import SisisUser
 
-# Create your tests here.
+
+class BlogTest(TestCase):
+    def setUp(self) -> None:
+        self.user = SisisUser.objects.create_user(email='pesho@abc.it', password='1234qwer')
+        self.writer = SisisUser.objects.create_user(email='gosho@abc.it', password='1234qwer')
+        self.client = Client()
+        self.post = Post.objects.create(title='test',
+                                        slug='test',
+                                        author=self.writer,
+                                        content='test content',
+                                        )
+
+    def test_everyone_can_see_post(self):
+        response = self.client.get('/blog/post/1/test')
+        self.assertTrue(response.context['post'], self.post)
+
+    def test_only_author_can_edit_his_post(self):
+        response = self.client.get('/blog/post-update/1/test')
+        self.assertEqual(response.status_code, 302)
+        self.assertTemplateUsed('accounts/login.html')
+
+        self.client.force_login(self.writer)
+        response = self.client.get('/blog/post-update/1/test')
+        self.assertEqual(response.status_code, 302)
+        self.assertTemplateUsed('blog/post-update.html')
+
+    def test_only_author_can_delete_his_post(self):
+        response = self.client.get('/blog/post-delete/1/test')
+        self.assertEqual(response.status_code, 302)
+        self.assertTemplateUsed('accounts/login.html')
+
+        self.client.force_login(self.writer)
+        response = self.client.get('/blog/post-delete/1/test')
+        self.assertEqual(response.status_code, 302)
+        self.assertTemplateUsed('blog/blog.html')
+
--- sisys/sisis_auth/tests.py
+++ sisys/sisis_auth/tests.py
@@ -1,37 +1,46 @@
 from django.contrib.auth import get_user_model
-from django.template import context
 from django.test import TestCase, Client
-from django.urls import reverse
+
 
 from sisis_auth.models import Profile
-
 UserModel = get_user_model()
 
 
 class CreateUserTest(TestCase):
 
-    def setUp(self):
+    def setUp(self) -> None:
         self.client = Client()
         self.email = 'pesho@abc.it'
         self.password = '1234qwer'
 
-    def test_check_createdUser__shouldHaveProfile(self):
+    def test_check_createdUser__shouldCreateProfileToo(self):
         self.user = UserModel.objects.create_user(email=self.email, password=self.password)
         self.client.force_login(self.user)
         self.profile = Profile.objects.get(pk=self.user.id)
         self.assertEqual(self.user.id, self.profile.user_id)
 
+    def test_check_userCreated(self):
+        response = self.client.post('/accounts/register/?next=/accounts/email-confirmation/', kwargs={'email': self.email, 'password1': self.password, 'password2': self.password})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed('accounts/activate-your-account.html')
 
-class ViewAccountTest(TestCase):
+
+class ProfileTest(TestCase):
     def setUp(self) -> None:
         self.client = Client()
         self.user = UserModel.objects.create_user(email='pesho@abc.it', password='1234qwer')
 
-    def test_userShouldSeeAccount__whenLoggedIn(self):
-        self.client.login(email='pesho@abc.it', password='1234qwer')
-        response = self.client.get(reverse('my account'))
-        profile = response.context['profile']
-        print(profile)
+    def test_profile_rendered_in_correct_template(self):
+        self.client.force_login(self.user)
+        self.client.get('/accounts/profile/')
+        self.assertTemplateUsed('accounts/profile_details.html')
+
+
+
+
+
+
+
 
 
 
